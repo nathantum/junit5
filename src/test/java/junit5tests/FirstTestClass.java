41baa3feb929e4246e00796ce04af5a3ef1e307c
nathantum <tumkipsang@gmail.com>
2023-03-05 07:33:28 -0600

    added parameterized test methods @ValueSource

--- src/test/java/junit5tests/FirstTestClass.java
+++ src/test/java/junit5tests/FirstTestClass.java
@@ -1,10 +1,28 @@
 package junit5tests;
 
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class FirstTestClass {
+    @BeforeAll
+    void beforeAll(){
+        System.out.println("-- This is the before ALL method");
+    }
+
+    @BeforeEach
+    void beforeEach(){
+        System.out.println("---- this is the before EACH method");
+    }
+
+    @AfterAll
+    void afterAll(){
+        System.out.println("-- This is after All method");
+    }
+
+    @AfterEach
+    void afterEach(){
+        System.out.println("---- this is the after EACH method");
+    }
     @Test
     void firstMethod(){
         System.out.println("this is the first test method");
